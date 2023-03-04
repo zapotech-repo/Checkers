bb65bcf43cde63ed0d7ee54caa1c5c15d7136bf7
Kai Parris <kai.parris2018@gmail.com>
2023-03-03 20:07:31 -0700

    Inital Commit

---
+++ analasis.py
@@ -0,0 +1,51 @@
+from numpy import copy
+
+
+def get_moves(p):
+    if p == 1:
+        return [(1, -1), (1, 1)]
+    if p == 2:
+        return [(-1, -1), (-1, 1)]
+    return [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+
+def good(bd, x, y):
+    if not (0 <= x < 8) or not (0 <= y < 8):
+        return False
+    return not bd[x, y]
+
+
+def can_cap(bd, p, x, y, vx, vy):
+    if not (0 <= x < 8) or not (0 <= y < 8):
+        return False
+    if bd[x, y] in [0, abs(p)]:
+        return False
+    return good(bd, x + vx, y + vy)
+
+
+def move(bd, x, y, cap, mv):
+    for dx, dy in get_moves(bd[x, y]):
+        nx, ny = x + dx, y + dy
+        if good(bd, nx, ny) and not cap:
+            mv.append((nx, ny, tuple()))
+        elif can_cap(bd, bd[x, y], nx, ny, nx + dx, ny + dy):
+            c = cap + ((nx, ny),)
+            mv.append((nx, ny, c))
+            cb = copy(bd)
+            cb[x, y] = 0
+            cb[nx, ny] = 0
+            cb[nx + dx, ny + dy] = bd[x, y]
+            move(cb, nx + dx, ny + dy, c, mv)
+
+
+def all_moves(bd):
+    ml = {}
+    for x in range(8):
+        for y in range(8):
+            if not bd[x, y]:
+                continue
+            l = []
+            move(bd, x, y, tuple(), l)
+            if l:
+                ml[(x, y)] = l
+    return ml
---
+++ main.py
@@ -0,0 +1,42 @@
+from numpy import asarray
+from tkinter import *
+from analasis import *
+
+
+board = asarray(
+    [
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [-1, 0, -1, 0, -1, 0, -1, 0],
+        [0, -1, 0, -1, 0, -1, 0, -1],
+        [-1, 0, -1, 0, -1, 0, -1, 0],
+    ]
+)
+
+
+cp = ["#b1e4b9","#70a2a3"]
+ac = ["#bfe3c4","#7aa3a3"]
+class Game:
+    can: Canvas
+
+    def __init__(self):
+        self.root = Tk()
+        self.draw_ui()
+
+    def draw_ui(self):
+        self.can = Canvas(self.root, highlightthickness=0)
+        self.can.place(x=0,y=0,width=512,height=512)
+        for x in range(8):
+            for y in range(8):
+                self.can.create_rectangle(64*x,64*y,64*x+64,64*y+64,fill=cp[(x+y)%2],activefill = ac[(x+y)%2],outline="black")
+        self.root.geometry("512x512")
+
+    def run(self):
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    Game().run()
